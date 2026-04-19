007109b0981687b69fccb3e8b17a8fe4a01db2b1
thonk <hello@villainsrule.xyz>
2026-04-19 18:37:48 -0400

    new path resolution mechanism

--- src/pull.ts
+++ src/pull.ts
@@ -2,13 +2,11 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 const pullFile = async (p: string) => {
-    if (p.startsWith('/') || p.startsWith('./') || p.startsWith('../')) {
-        const filePath = path.resolve(p);
-        if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) return fs.readFileSync(filePath, 'utf8');
-        else throw new Error(`file not found on local FS: ${p}`);
-    } else {
+    const filePath = path.resolve(p);
+    if (fs.existsSync(filePath) && fs.statSync(filePath).isFile()) return fs.readFileSync(filePath, 'utf8');
+    else {
         const req = await fetch(p);
-        if (!req.ok) throw new Error(`failed to fetch ${p}: ${req.status} ${req.statusText}`);
+        if (!req.ok) throw new Error(`failed to find ${p} on local FS or web`);
         return await req.text();
     }
 }
